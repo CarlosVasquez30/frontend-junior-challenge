25a97da181303fc6df6eee2a7cae701e47ad5727
CarlosDev16 <carlos.vasquez@sellside.cl>
2023-03-13 18:07:18 -0300

    Revert "commit"
    This reverts commit 97c353a20dbe077c8c4296fcb1f7b7f81b15fbd3.

--- src/components/TodoResults/index.jsx
+++ src/components/TodoResults/index.jsx
@@ -1,24 +1,10 @@
 import React from "react";
 import "./styles.css";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchTodos } from "../../store/slices/todo";
-import { useEffect } from "react";
 
 const TodoResults = () => {
   // Fix an ability to calculate completed tasks
-  const todos = useSelector((state) => state.todos.todos);
-  const status = useSelector((state) => state.todos.status);
-  const error = useSelector((state) => state.todos.error);
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(fetchTodos());
-  }, [dispatch]);
-
-  const tareasRealizadas = todos.filter((todo) => todo.checked === true);
-  const string_tareasRealizadas_cont = `Tareas realizadas: ${tareasRealizadas.length}`;
-
-  return <div className="todo-results">{string_tareasRealizadas_cont}</div>;
+  return <div className="todo-results">Done:</div>;
 };
 
 export default TodoResults;
